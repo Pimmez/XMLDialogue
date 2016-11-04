860aecb35f3a78ebf4d92d79de554050e56f42b8
Pim Horeman <pimhoreman@gmail.com>
2016-11-04 20:38:44 +0100

    First Commit

---
+++ Assets/Scripts/ChangeButtonText.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChangeButtonText : MonoBehaviour {
+
+    [SerializeField]
+    private Text uiText;
+
+    public void ChangeText(string buttonText)
+    {
+        uiText.text = buttonText;
+    }
+
+}
---
+++ Assets/Scripts/SuperTest.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuperTest : MonoBehaviour {
+
+    private XMLReader xmlReader = new XMLReader();
+
+    [SerializeField]
+    private GameObject button;
+
+    [SerializeField]
+    private string file;
+
+    [SerializeField]
+    private string path;
+
+    [SerializeField]
+    private string node;
+
+    [SerializeField]
+    private int id;
+
+    // Use this for initialization
+    void Start () {
+        xmlReader.READXML(file, path, node, id);
+        Debug.Log(xmlReader.READXML(file, path, node, id));
+        //xmlReader.ReadSubnodes(file, path, id);
+        //Debug.Log(xmlReader.ReadSubnodes(file, path, id));
+
+        string[] choices = xmlReader.ReadSubnodes(file, path, id);
+
+        for (int i = 0; i < xmlReader.ReadSubnodes(file, path + "/Greetings", id).Length; i++)
+        {
+            var choiceButton = (GameObject)Instantiate(button, new Vector3(0,i * 40,0), Quaternion.identity);
+            ChangeButtonText textChange = choiceButton.GetComponent<ChangeButtonText>();
+            choiceButton.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform,false);
+            textChange.ChangeText(xmlReader.ReadSubnodes(file, path + "/Greetings", id)[i]);
+            Debug.Log(xmlReader.ReadSubnodes(file, path + "/Greetings", id)[i]);
+        }
+
+    }
+	
+}
